1d3be495781c6478b22c0c127b2e37f560d884c2
Elquintas <sebastiao.frazao@gmail.com>
2023-12-29 10:37:51 +0100

    area-weighted means instead of regular means

--- dataloader/load.py
+++ dataloader/load.py
@@ -63,36 +63,65 @@ class load_data(Dataset):
         self.s_2000 = xy.values[:,42]
         self.s_4000 = xy.values[:,43]
 
+        # Room dimensions and surface area 
+
         self.room_x = xy.values[:,44]
         self.room_y = xy.values[:,45]
         self.room_z = xy.values[:,46]
 
+        # Get area of each showbox wall, floor and ceiling
+        self.area_ns = (self.room_x*self.room_z)
+        self.area_ew = (self.room_y*self.room_z)
+        self.area_cf = (self.room_x*self.room_y)
+
+        # Get total surface area of the room
+        self.area_tot = (self.area_ns)*2 + (self.area_ew)*2 + (self.area_cf)*2
+
+        # Get the coefficients for the weighted mean of the surface areas
+        self.coef_n = self.coef_s = self.area_ns / self.area_tot
+        self.coef_e = self.coef_w = self.area_ew / self.area_tot
+        self.coef_c = self.coef_f = self.area_cf / self.area_tot
+
+        # Parses source location
         self.source_x = xy.values[:,47]
         self.source_y = xy.values[:,48]
         self.source_z = xy.values[:,49]
 
+        # Parses microphone location
         self.micro_x = xy.values[:,50]
         self.micro_y = xy.values[:,51]
         self.micro_z = xy.values[:,52]
 
         self.n_samples = xy.shape[0]
 
-        # Get mean absorption coefficients for each frequency band
-        # accross all 6-dim of a showbox model
-        self.mean_125 = (self.c_125+self.f_125+self.w_125+
-                self.e_125+self.s_125+self.n_125)/6
-        self.mean_250 = (self.c_250+self.f_250+self.w_250+
-                self.e_250+self.s_250+self.n_250)/6
-        self.mean_500 = (self.c_500+self.f_500+self.w_500+
-                self.e_500+self.s_500+self.n_500)/6
-        self.mean_1000 = (self.c_1000+self.f_1000+self.w_1000+
-                self.e_1000+self.s_1000+self.n_1000)/6
-        self.mean_2000 = (self.c_2000+self.f_2000+self.w_2000+
-                self.e_2000+self.s_2000+self.n_2000)/6
-        self.mean_4000 = (self.c_4000+self.f_4000+self.w_4000+
-                self.e_4000+self.s_4000+self.n_4000)/6
+        
+        # Parses the area-weighted mean absorption coefficients 
+        # for each frequency band across all 6-dims
+        self.mean_125 = self.c_125*self.coef_c+self.f_125*self.coef_f+\
+                        self.e_125*self.coef_e+self.w_125*self.coef_w+\
+                        self.n_125*self.coef_n+self.s_125*self.coef_s
 
-                
+        self.mean_250 = self.c_250*self.coef_c+self.f_250*self.coef_f+\
+                        self.e_250*self.coef_e+self.w_250*self.coef_w+\
+                        self.n_250*self.coef_n+self.s_250*self.coef_s     
+    
+        self.mean_500 = self.c_500*self.coef_c+self.f_500*self.coef_f+\
+                        self.e_500*self.coef_e+self.w_500*self.coef_w+\
+                        self.n_500*self.coef_n+self.s_500*self.coef_s
+
+        self.mean_1000 = self.c_1000*self.coef_c+self.f_1000*self.coef_f+\
+                        self.e_1000*self.coef_e+self.w_1000*self.coef_w+\
+                        self.n_1000*self.coef_n+self.s_1000*self.coef_s
+
+        self.mean_2000 = self.c_2000*self.coef_c+self.f_2000*self.coef_f+\
+                        self.e_2000*self.coef_e+self.w_2000*self.coef_w+\
+                        self.n_2000*self.coef_n+self.s_2000*self.coef_s
+
+        self.mean_4000 = self.c_4000*self.coef_c+self.f_4000*self.coef_f+\
+                        self.e_4000*self.coef_e+self.w_4000*self.coef_w+\
+                        self.n_4000*self.coef_n+self.s_4000*self.coef_s
+
+
 
     def __len__(self):
         return self.n_samples
