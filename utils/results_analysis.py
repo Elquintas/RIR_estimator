f52d30659d56f1904e56ef78cc24b1ed4a134262
Elquintas <sebastiao.frazao@gmail.com>
2024-01-01 17:25:14 +0100

    baselines added

--- utils/results_analysis.py
+++ utils/results_analysis.py
@@ -1,4 +1,5 @@
 import sys
+import math
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -22,11 +23,16 @@ def main():
 
     # Gets sabine coefficients from theoretical rt60.
     sab_coefs=[]
+    eyr_coefs=[]
     for i in range(len(df_t)):
         sab = pra.inverse_sabine(df_t['rt60'][i],
                 [df_t['room_x'][i],df_t['room_y'][i],df_t['room_z'][i]])
         sab_coefs.append(sab[0])
+        eyr = -math.log(1-sab[0])
+        eyr_coefs.append(eyr)
 
+    sab_coefs = pd.DataFrame(sab_coefs)
+    eyr_coefs = pd.DataFrame(eyr_coefs)
 
 
     # Absolute error
@@ -37,6 +43,21 @@ def main():
     band5 = (df['ref_2000Hz']-df['pred_2000Hz']).abs()
     band6 = (df['ref_4000Hz']-df['pred_4000Hz']).abs()
 
+    # Full band (average) - REFERENCE
+    ref_bands = (df['ref_125Hz']+df['ref_250Hz']+df['ref_500Hz']+
+                df['ref_1000Hz']+df['ref_2000Hz']+df['ref_4000Hz'])/6
+
+    pred_bands = (df['pred_125Hz']+df['pred_250Hz']+df['pred_500Hz']+
+                df['pred_1000Hz']+df['pred_2000Hz']+df['pred_4000Hz'])/6
+
+    avg_coefs_pred = (ref_bands - pred_bands).abs() 
+
+    avg_coefs_sab = (ref_bands - sab_coefs[0]).abs()
+
+    avg_coefs_eyr = (ref_bands - eyr_coefs[0]).abs()
+
+
+
     dist_x = (df['ref_x']-df['pred_x']).abs()
     dist_y = (df['ref_y']-df['pred_y']).abs()
     dist_z = (df['ref_z']-df['pred_z']).abs()
@@ -45,12 +66,15 @@ def main():
 
 
     # Plots error bars - absorption coefficients
-    bands = ['125Hz','250Hz','500Hz','1000Hz','2000Hz','4000Hz']
+    bands = ['125Hz','250Hz','500Hz','1000Hz','2000Hz','4000Hz',
+             'All bands','Sabine','Eyring']
     x_pos = np.arange(len(bands))
     CTEs = [band1.mean(),band2.mean(),band3.mean(),
-            band4.mean(),band5.mean(),band6.mean(),]
+            band4.mean(),band5.mean(),band6.mean(),
+            avg_coefs_pred.mean(),avg_coefs_sab.mean(),avg_coefs_eyr.mean()]
     error = [band1.std(),band2.std(),band3.std(),
-            band4.std(),band5.std(),band6.std(),]
+            band4.std(),band5.std(),band6.std(),
+            avg_coefs_pred.std(),avg_coefs_sab.std(),avg_coefs_eyr.std()]
     fig, ax = plt.subplots()
     ax.bar(x_pos, CTEs, yerr=error, align='center', alpha=0.5, 
             ecolor='black', capsize=10)
